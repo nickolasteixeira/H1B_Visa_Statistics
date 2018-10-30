e3bb56318bc362cd9a94e121b254fe64f342b1e1
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-10-30 01:46:48 +0000

    change back to standard input for testing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use this to run the script on the sample h1b_input.csv
-#python3 ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python3 ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
 # I downloaded all the files from https://www.foreignlaborcert.doleta.gov/performancedata.cfm and listed them into my host folder. Unfortunately I was not able to host the large csv files onto github, so I had to figure a workout. Since I'm testing my application code in a Vagrant box, I had to download the files locally on my host and then vagrant scp them into my box. I could then test the files on my vagrant box
 # If you do have the additional files (ex: files provided from the link you posted on the github readme of the challenge link: https://drive.google.com/drive/folders/1Nti6ClUfibsXSQw5PUIWfVGSIrpuwyxf, feel free to uncomment out the lines below to test the code
@@ -8,7 +8,7 @@
 # To run multiple files in the input folder uncomment files below
 #python3 ./src/h1b_counting.py ./input/H1B_FY_2014.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 #python3 ./src/h1b_counting.py ./input/H1B_FY_2015.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
-python3 ./src/h1b_counting.py ./input/H1B_FY_2016.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+#python3 ./src/h1b_counting.py ./input/H1B_FY_2016.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
 
 # I also downloaded the rest of the years from https://www.foreignlaborcert.doleta.gov/performancedata.cfm
