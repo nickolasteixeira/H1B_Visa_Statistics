e6daa63b290a41db2b26a7560f4f1fed7462e459
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-10-29 21:37:45 -0700

    unit tests, add yearly_headers back in file

---
+++ insight_testsuite/tests/test_h1b_counting.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+'''Unit tests for all functions in h1b_counting'''
+from src.h1b_counting import check_parameters, yearly_headers, find_year, open_file, sort_dictionaries, write_occupation, write_state
+import os
+import unittest
+import subprocess
+
+
+class TestData(unittest.TestCase):
+    '''testing all functions in src/h1b_counting.py class'''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.occ_test = {'SOFTWARE DEVELOPERS, APPLICATIONS': 6, 'ACCOUNTANTS AND AUDITORS': 1, 'COMPUTER SYSTEMS ANALYST': 1, 'total': 10, 'COMPUTER OCCUPATIONS, ALL OTHER': 1, 'DATABASE ADMINISTRATORS': 1}
+        cls.state_test = {'AL': 1, 'GA': 1, 'NJ': 1, 'CA': 1, 'FL': 2, 'total': 10, 'WA': 1, 'TX': 1, 'MD': 1, 'DE': 1}
+        cls.input_file = 'h1b_input.csv'
+        cls.input_path = './input'
+        cls.header = yearly_headers['2017']
+     
+    @classmethod
+    def tearDownClass(cls):
+        del cls.occ_test
+        del cls.state_test
+        del cls.input_file
+        del cls.input_path
+        del cls.header
+
+    def test_checking_docstrings_functions(self):
+        self.assertIsNotNone(check_parameters.__doc__)
+        self.assertIsNotNone(find_year.__doc__)
+        self.assertIsNotNone(open_file.__doc__)
+
+
+    def test_find_year(self):
+        input_file = 'H1B_FY_2017.csv'
+        input_file2 = 'H1B_FY_2016.csv'
+        input_file3 = 'h1b_input.csv'
+        self.assertEqual(find_year(input_file), '2017')
+        self.assertEqual(find_year(input_file2), '2016')
+        self.assertEqual(find_year(input_file3), '2017')
+
+
+    def test_sort(self):
+        arr = []
+        arr.append(sorted(self.occ_test.items(), key=lambda x: (-x[1], x[0])))
+        arr.append(sorted(self.occ_test.items(), key=lambda x: (-x[1], x[0])))
+
+        length = len(arr[0]) - 1
+        for da_list in arr:
+            for index in range(len(da_list)):
+                if index < length:
+                    self.assertTrue(da_list[index][1] >= da_list[index + 1][1])
+    
+    def test_open_file(self):
+        occ, state = open_file(self.input_path, self.input_file, self.header)
+        self.assertDictEqual(self.occ_test, occ)
+        self.assertDictEqual(self.state_test, state) 
+
+if __name__ == '__main__':
+    unittest.main()
--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -5,7 +5,20 @@ import heapq
 import os
 import re
 import sys
-from yearly_headers import yearly_headers
+
+#Each year, record header laytouts change. This is dictionary that mimics the header from each year. Refer to LCA Programs (H1-B, H-1B1, E-3) https://www.foreignlaborcert.doleta.gov/performancedata.cfm
+yearly_headers = {
+    '2017': ['CASE_STATUS', 'SOC_NAME', 'WORKSITE_STATE'],
+    '2016': ['CASE_STATUS', 'SOC_NAME', 'WORKSITE_STATE'],
+    '2015': ['CASE_STATUS', 'SOC_NAME', 'WORKSITE_STATE'],
+    '2014': ['STATUS', 'LCA_CASE_SOC_NAME', 'LCA_CASE_WORKLOC1_STATE'],
+    '2013': ['STATUS', 'LCA_CASE_SOC_NAME', 'LCA_CASE_WORKLOC1_STATE'],
+    '2012': ['STATUS', 'LCA_CASE_SOC_NAME', 'LCA_CASE_WORKLOC1_STATE'],
+    '2011': ['STATUS', 'LCA_CASE_SOC_NAME', 'LCA_CASE_WORKLOC1_STATE'],
+    '2010': ['STATUS', 'LCA_CASE_SOC_NAME', 'WORK_LOCATION_STATE2'],
+    '2009': ['STATUS', 'LCA_CASE_SOC_NAME', 'LCA_CASE_WORKLOC1_STATE'],
+    '2008': ['APPROVAL_STATUS', 'OCCUPATIONAL_TITLE', 'STATE_2']
+}
 
 def check_parameters(params):
     '''
